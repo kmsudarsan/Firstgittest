d7fd76b0c5d8e1d64d9b065d2c1bf12c3cd260ef
KM <kmsudarsanam1981@gmail.com>
2016-05-14 21:36:03 +0530

    last commit

--- Testjavaprj/src/Testhello.java
+++ Testjavaprj/src/Testhello.java
@@ -4,7 +4,7 @@ public class Testhello {
 public static void main (String Args[])
 {
 	System.out.println("Hhjheddsdllo");
-	System.out.println("Hhjheddsdllo");
+	System.out.println("knhkhkhk");
 
 	//LOG.info("The message");
 }
